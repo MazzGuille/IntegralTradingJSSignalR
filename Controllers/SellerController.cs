267749c4e4c57d4601171155f3ba87a7ff778de6
MazzCode <mazzcode@gmail.com>
2023-01-26 17:53:45 -0300

    agregada vista vendedor

---
+++ Controllers/SellerController.cs
@@ -0,0 +1,120 @@
+﻿using IntegralTradingJS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Collections.Generic;
+
+namespace IntegralTradingJS.Controllers
+{
+    public class SellerController : Controller
+    {
+        public List<HviRed> HviData = new();
+       
+        public ActionResult OfferUpload()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public IActionResult MostrarDatos([FromForm] IFormFile ArchivoExcel)
+        {
+
+            Stream stream = ArchivoExcel.OpenReadStream();
+
+            IWorkbook MiExcel = null;
+
+            if (Path.GetExtension(ArchivoExcel.FileName) == ".xlsx")
+            {
+                MiExcel = new XSSFWorkbook(stream);
+            }
+            else
+            {
+                MiExcel = new HSSFWorkbook(stream);
+            }
+
+            ISheet HojaExcel = MiExcel.GetSheetAt(0);
+
+            int cantidadFila = HojaExcel.LastRowNum;
+            for (int i = 1; i < cantidadFila; i++)
+            {
+                IRow fila = HojaExcel.GetRow(i);
+                HviData.Add(new HviRed
+                {
+                    UHML = fila.GetCell(0).ToString(),
+                    UI = fila.GetCell(1).ToString(),
+                    Strength = fila.GetCell(2).ToString(),
+                    SFI = fila.GetCell(3).ToString(),
+                    Mic = fila.GetCell(4).ToString(),
+                    ColorGrade = fila.GetCell(5).ToString(),
+                    TrashID = fila.GetCell(6).ToString(),
+                });
+            }
+            return StatusCode(StatusCodes.Status200OK, HviData);
+        }
+
+        // GET: SellerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SellerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SellerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SellerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SellerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SellerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/HviRed.cs
@@ -0,0 +1,13 @@
+﻿namespace IntegralTradingJS.Models
+{
+    public class HviRed
+    {
+        public string UHML { get; set; }
+        public string UI { get; set; }
+        public string Strength { get; set; }
+        public string SFI { get; set; }
+        public string Mic { get; set; }
+        public string ColorGrade { get; set; }
+        public string TrashID { get; set; }
+    }
+}
