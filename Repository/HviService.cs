0c3fd72de8a6d52fdf65a67b1d7dde87473d72b3
JesusMunoz98 <85593226+JesusMunoz98@users.noreply.github.com>
2023-02-08 23:01:57 -0600

    Intento insert

--- Repository/HviService.cs
+++ Repository/HviService.cs
@@ -53,6 +53,50 @@ namespace IntegralTradingJS.Repository
             return hviList;
         }
 
+        private void InsertData(int ID, int WhseCode, int WhseTag, int C1, int C2, int Leaf, int Mic, float Str, float LRR, int CropYear, int NetWeight, String Comp, int Len, int Ext,
+            float RD, int PlusB, float Uni, int Trash, int StorageDate)
+        {
+            // define INSERT query with parameters
+            string query = "INSERT INTO HVIcsv (ID, WhseCode, WhseTag, C1, C2, Leaf,Mic,Str,LRR,CropYear,NetWeight,Comp,Len,Ext,RD,PlusB,Uni,Trash,StorageDate) " +
+                           "VALUES (@ID,@WhseCode,@WhseTag,@C1,@C2,@Leaf,@Mic,@Str,@LRR,@CropYear,@NetWeight,@Comp,@Len,@Ext,@RD,@PlusB,@Uni,@Trash,@StorageDate) ";
+
+            /*string query2 = "INSERT INTO HVIcsv VALUES ("+ID+','+WhseCode+','+WhseTag+','+C1+','+C2+','+Leaf+','+Mic+','+Str+','+LRR+','+CropYear+','+NetWeight+','+Comp+','+Len+','+Ext+','+
+                RD+','+PlusB+','+Uni+','+Trash+','+StorageDate+");";*/
+
+            // create connection and command
+            using (SqlConnection cn = new SqlConnection(sqlString.GetSqlString()))
+            using (SqlCommand cmd = new SqlCommand(query, cn))
+            {
+                // define parameters and their values
+
+                cmd.Parameters.AddWithValue("@ID", ID);
+                cmd.Parameters.AddWithValue("@WhseCode", WhseCode);
+                cmd.Parameters.AddWithValue("@WhsetAG", WhseTag);
+                cmd.Parameters.AddWithValue("@C1", C1);
+                cmd.Parameters.AddWithValue("@C2", C2);
+                cmd.Parameters.AddWithValue("@Leaf", Leaf);
+                cmd.Parameters.AddWithValue("@Mic", Mic);
+                cmd.Parameters.AddWithValue("@Str", Str);
+                cmd.Parameters.AddWithValue("@LRR", LRR);
+                cmd.Parameters.AddWithValue("@CropYear", CropYear);
+                cmd.Parameters.AddWithValue("@NetWeight", NetWeight);
+                cmd.Parameters.AddWithValue("@Comp", Comp);
+                cmd.Parameters.AddWithValue("@Len", Len);
+                cmd.Parameters.AddWithValue("@Ext", Ext);
+                cmd.Parameters.AddWithValue("@RD", RD);
+                cmd.Parameters.AddWithValue("@PlusB", PlusB);
+                cmd.Parameters.AddWithValue("@Uni", Uni);
+                cmd.Parameters.AddWithValue("@Trash", Trash);
+                cmd.Parameters.AddWithValue("@StorageDate", StorageDate);
+
+                // open connection, execute INSERT, close connection
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                cn.Close();
+            }
+
+        }
+
 
     }
 }
