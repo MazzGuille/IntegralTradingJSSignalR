b1daaaeae8650e3e3219855cd2b2b2c00a8a4972
MazzCode <mazzcode@gmail.com>
2023-02-27 11:41:54 -0300

    ok

--- Helpers/Encrypt.cs
+++ Helpers/Encrypt.cs
@@ -1,6 +1,26 @@
-﻿namespace IntegralTradingJS.Helpers
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace IntegralTradingJS.Helpers
 {
     public class Encrypt
     {
+        public string ConvertirSHA256(string text)
+        {
+            if (text == null)
+            {
+                return null;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            using (SHA256 hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(text));
+                foreach (byte b in result)
+                    sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
+        }
     }
 }
