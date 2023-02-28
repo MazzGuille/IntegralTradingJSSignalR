c8653cdd431034b756b21b56e0041241f2ecf9b3
MazzCode <mazzcode@gmail.com>
2023-02-28 19:10:10 -0300

    id

--- Models/Login.cs
+++ Models/Login.cs
@@ -2,6 +2,7 @@
 {
     public class Login
     {
+        public int Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public int UserId { get; set; }
